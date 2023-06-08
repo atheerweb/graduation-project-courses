0ce13e698b521b48ff387d517d6eb751721796be
Aamerov <aamersyrai2000@gmail.com>
2023-06-08 15:11:48 +0300

    Added Cart Navigator & Added Dropper For the Courses Categories

--- components/layouts/MultipageLayout/components/Header.jsx
+++ components/layouts/MultipageLayout/components/Header.jsx
@@ -1,89 +1,135 @@
 // Components
-import Menu from "./Menu";
+import Menu from './Menu';
 // MUI Components
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputAdornment from "@mui/material/InputAdornment";
-import Link from "@mui/material/Link";
-import IconButton from "@mui/material/IconButton";
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Badge } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 // Next Components
-import Image from "next/image";
+import Image from 'next/image';
 // Hooks
-import { useState } from "react";
-import { useTheme } from "@mui/material/styles";
-import { useSelector } from "react-redux";
-import { useRouter } from "next/router";
+import { Fragment, useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 import useMediaQuery from '@mui/material/useMediaQuery';
 // CSS Modules
 import styles from '@/styles/modules/layouts/MultipageLayout/layout.module.css';
-
+import CustomizedMenus from './Dropper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 const Header = () => {
-    const navLinks = useSelector(state => state.constants.value.navLinks);
-    const theme = useTheme();
-    const router = useRouter();
-    const media = useMediaQuery("(max-width: 1100px)");
+  const navLinks = useSelector((state) => state.constants.value.navLinks);
+  const theme = useTheme();
+  const router = useRouter();
+  const media = useMediaQuery('(max-width: 1100px)');
 
-    const [display, setDisplay] = useState("none");
+  const [display, setDisplay] = useState('none');
 
-    const handleMenuClick = () => {
-        setDisplay(previous => previous === "none" ? "flex" : "none");
-    }
+  const handleMenuClick = () => {
+    setDisplay((previous) => (previous === 'none' ? 'flex' : 'none'));
+  };
 
-    return (
-        <Box className={styles.mainNav} sx={{backgroundColor: theme.palette.accent.light, boxShadow: `0 0 5px ${theme.palette.accent.primary}`}}>
-            <Menu display={display} />
-            <Image width={62} height={41} src="/images/header_logo.png" alt="علمنى" priority />
-            <Stack direction="row" gap="65px" display={media && "none"}>
-                {
-                    navLinks.map((link, index) => (
-                        <Link
-                            className={styles.navLinks}
-                            sx={{
-                                color: link.href === router.pathname ? theme.palette.accent.dark : theme.palette.accent.primary,
-                                fontWeight: link.href === router.pathname && "bold",
-                                textDecoration: "none"
-                            }}
-                            key={index}
-                            href={link.href}
-                        >
-                            {link.title}
-                        </Link>        
-                    ))
-                }
-            </Stack>
-            <OutlinedInput
-                className={styles.mainNavSearch}
-                variant="outlined"
-                placeholder="ابحث عن مستقلين"
-                dir="ltr"
-                inputProps={{style: { textAlign: "end", borderRadius: "25px" }}}
-                sx={{display: media && "none", borderRadius: "25px"}}
-                startAdornment={
-                    <InputAdornment position="start">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          edge="start"
-                        >
-                            <SearchIcon color="primary" />
-                        </IconButton>
-                    </InputAdornment>
-                }
-            />
-            <Stack direction="row" gap="20px" display={media && "none"}>
-                <Link href="/signup">
-                    <Button variant="outlined" color="secondary">إنشاء حساب</Button>
-                </Link>
-                <Link href="/signin">
-                    <Button variant="contained" color="secondary">تسجيل دخول</Button>
-                </Link>
-            </Stack>
-            <MenuIcon sx={{color: theme.palette.primary.main, cursor: "pointer", display: !media && "none"}} onClick={handleMenuClick} />
-        </Box>
-    )
-}
+  const coursesPages = useSelector((state) => state.constants.value.dropItems);
+
+  return (
+    <Box
+      className={styles.mainNav}
+      sx={{
+        backgroundColor: theme.palette.accent.light,
+        boxShadow: `0 0 5px ${theme.palette.accent.primary}`,
+      }}
+    >
+      <Menu display={display} />
+      <Image
+        width={62}
+        height={41}
+        src="/images/header_logo.png"
+        alt="علمنى"
+        priority
+      />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          gap: 65,
+        }}
+      >
+        <Link
+          className={styles.navLinks}
+          sx={{
+            color:
+              '/' === router.pathname
+                ? theme.palette.accent.dark
+                : theme.palette.accent.primary,
+            fontWeight: '/' === router.pathname && 'bold',
+            textDecoration: 'none',
+            p: 0,
+            m: 0,
+          }}
+          href="/"
+        >
+          الرئيسية
+        </Link>
+        <CustomizedMenus data={coursesPages} name={'مسارات'} />
+      </div>
+      <OutlinedInput
+        className={styles.mainNavSearch}
+        variant="outlined"
+        placeholder="ابحث عن مستقلين"
+        dir="ltr"
+        inputProps={{ style: { textAlign: 'end', borderRadius: '25px' } }}
+        sx={{ display: media && 'none', borderRadius: '25px' }}
+        startAdornment={
+          <InputAdornment position="start">
+            <IconButton aria-label="toggle password visibility" edge="start">
+              <SearchIcon color="primary" />
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      <Stack direction="row" gap="20px" display={media && 'none'}>
+        <Link href="/cart">
+          <IconButton color="secondary" aria-label="add to shopping cart">
+            <Badge badgeContent={1} color="primary">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        </Link>
+        <Link href="/signup">
+          <Button variant="text" color="primary">
+            إنشاء حساب
+          </Button>
+        </Link>
+        <Link href="/signin">
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              color: theme.palette.accent.light,
+            }}
+          >
+            تسجيل دخول
+          </Button>
+        </Link>
+      </Stack>
+      <MenuIcon
+        sx={{
+          color: theme.palette.primary.main,
+          cursor: 'pointer',
+          display: !media && 'none',
+        }}
+        onClick={handleMenuClick}
+      />
+    </Box>
+  );
+};
 
 export default Header;
--- redux/slices/constantsSlice.js
+++ redux/slices/constantsSlice.js
@@ -1,95 +1,143 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { Twitter, LinkedIn, Facebook, Instagram, DifferenceOutlined, PeopleAltOutlined, QuestionAnswerOutlined, VerifiedUserOutlined, BusinessCenterOutlined, PersonOutlineOutlined  } from "@mui/icons-material";
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  Twitter,
+  LinkedIn,
+  Facebook,
+  Instagram,
+  DifferenceOutlined,
+  PeopleAltOutlined,
+  QuestionAnswerOutlined,
+  VerifiedUserOutlined,
+  BusinessCenterOutlined,
+  PersonOutlineOutlined,
+} from '@mui/icons-material';
 
 export const constantsSlice = createSlice({
-    name: "constants",
-    initialState: {
-        value: {
-            navLinks: [
-                {title: "مستقلين", href: "/freelancers"},
-                {title: "وظائف", href: "/jobs"},
-                {title: "الرئيسية", href: "/"}
-            ],
-            footerLinks: [
-                {title: "أسئلة شائعة", href: "/FAQ"},
-                {title: "عنا", href: "/about"},
-                {title: "مستقلين", href: "/freelancers"},
-                {title: "وظائف", href: "/jobs"},
-                {title: "الرئيسية", href: "/"}
-            ],
-            footerIcons: (color) => (
-                [
-                    <Facebook key={0} sx={{color: color, cursor: "pointer"}} />,
-                    <Twitter key={1} sx={{color: color, cursor: "pointer"}} />,
-                    <LinkedIn key={2} sx={{color: color, cursor: "pointer"}} />,
-                    <Instagram key={3} sx={{color: color, cursor: "pointer"}} />
-                ]
-            ),
-            testimonialsCards: [
-                {title: "يوسف مصطفي", subTitle: "مبرمج فرونت أند", body: "“للبقاء في طليعة ابتكارات تكنولوجيا المعلومات ، يحتاج فريقك إلى إعادة ابتكار مجموعة مهاراته بانتظام. مع Udemy Business ، يمكنني منح فريقي مساحة للتعلم وأخذ زمام المبادرة. هذا يعني أنه يمكننا إنتاج أعمال عالية الجودة بسرعة أكبر .”"},
-                {title: "يوسف مصطفي", subTitle: "مبرمج فرونت أند", body: "“للبقاء في طليعة ابتكارات تكنولوجيا المعلومات ، يحتاج فريقك إلى إعادة ابتكار مجموعة مهاراته بانتظام. مع Udemy Business ، يمكنني منح فريقي مساحة للتعلم وأخذ زمام المبادرة. هذا يعني أنه يمكننا إنتاج أعمال عالية الجودة بسرعة أكبر .”"},
-                {title: "يوسف مصطفي", subTitle: "مبرمج فرونت أند", body: "“للبقاء في طليعة ابتكارات تكنولوجيا المعلومات ، يحتاج فريقك إلى إعادة ابتكار مجموعة مهاراته بانتظام. مع Udemy Business ، يمكنني منح فريقي مساحة للتعلم وأخذ زمام المبادرة. هذا يعني أنه يمكننا إنتاج أعمال عالية الجودة بسرعة أكبر .”"}
-            ],
-            coursesTracks: [
-                ["statistics", "إحصائي"],
-                ["robotics", "مهندس روبوتات"],
-                ["dataAnalysis", "علوم البيانات"],
-                ["accounting", "محاسبه"],
-                ["business", "إدارة اعمال"],
-                ["programming", "مهندس برمجيات"]
-            ],
-            coursesTracksCards: {
-                programming: [
-                    {title: "كورس البرمجة", subTitle: "مهندس برمجيات", ratings: 5},
-                    {title: "كورس البرمجة", subTitle: "مهندس برمجيات", ratings: 5},
-                    {title: "كورس البرمجة", subTitle: "مهندس برمجيات", ratings: 5},
-                    {title: "كورس البرمجة", subTitle: "مهندس برمجيات", ratings: 5},
-                    {title: "كورس البرمجة", subTitle: "مهندس برمجيات", ratings: 5},
-                    {title: "كورس البرمجة", subTitle: "مهندس برمجيات", ratings: 5}
-                ],
-                business: [
-                    {title: "كورس إدارة الأعمال", subTitle: "إدارة اعمال", ratings: 5},
-                    {title: "كورس إدارة الأعمال", subTitle: "إدارة اعمال", ratings: 5},
-                    {title: "كورس إدارة الأعمال", subTitle: "إدارة اعمال", ratings: 5},
-                    {title: "كورس إدارة الأعمال", subTitle: "إدارة اعمال", ratings: 5},
-                    {title: "كورس إدارة الأعمال", subTitle: "إدارة اعمال", ratings: 5},
-                    {title: "كورس إدارة الأعمال", subTitle: "إدارة اعمال", ratings: 5}
-                ],
-                accounting: [
-                    {title: "كورس المحاسبة", subTitle: "محاسبه", ratings: 5},
-                    {title: "كورس المحاسبة", subTitle: "محاسبه", ratings: 5},
-                    {title: "كورس المحاسبة", subTitle: "محاسبه", ratings: 5},
-                    {title: "كورس المحاسبة", subTitle: "محاسبه", ratings: 5},
-                    {title: "كورس المحاسبة", subTitle: "محاسبه", ratings: 5},
-                    {title: "كورس المحاسبة", subTitle: "محاسبه", ratings: 5}
-                ],
-                dataAnalysis: [
-                    {title: "كورس علوم البيانات", subTitle: "علوم البيانات", ratings: 5},
-                    {title: "كورس علوم البيانات", subTitle: "علوم البيانات", ratings: 5},
-                    {title: "كورس علوم البيانات", subTitle: "علوم البيانات", ratings: 5},
-                    {title: "كورس علوم البيانات", subTitle: "علوم البيانات", ratings: 5},
-                    {title: "كورس علوم البيانات", subTitle: "علوم البيانات", ratings: 5},
-                    {title: "كورس علوم البيانات", subTitle: "علوم البيانات", ratings: 5}
-                ],
-                robotics: [
-                    {title: "كورس الروبوتات", subTitle: "مهندس روبوتات", ratings: 5},
-                    {title: "كورس الروبوتات", subTitle: "مهندس روبوتات", ratings: 5},
-                    {title: "كورس الروبوتات", subTitle: "مهندس روبوتات", ratings: 5},
-                    {title: "كورس الروبوتات", subTitle: "مهندس روبوتات", ratings: 5},
-                    {title: "كورس الروبوتات", subTitle: "مهندس روبوتات", ratings: 5},
-                    {title: "كورس الروبوتات", subTitle: "مهندس روبوتات", ratings: 5}
-                ],
-                statistics: [
-                    {title: "كورس تحليل البيانات", subTitle: "إحصائي", ratings: 5},
-                    {title: "كورس تحليل البيانات", subTitle: "إحصائي", ratings: 5},
-                    {title: "كورس تحليل البيانات", subTitle: "إحصائي", ratings: 5},
-                    {title: "كورس تحليل البيانات", subTitle: "إحصائي", ratings: 5},
-                    {title: "كورس تحليل البيانات", subTitle: "إحصائي", ratings: 5},
-                    {title: "كورس تحليل البيانات", subTitle: "إحصائي", ratings: 5}
-                ],
-            }
-        }
-    }
+  name: 'constants',
+  initialState: {
+    value: {
+      navLinks: [{ title: 'الرئيسية', href: '/' }, { title: 'المسارات' }],
+      dropItems: [
+        { title: 'برمجة', href: '/' },
+        { title: 'إدارة', href: '/' },
+        { title: 'محاسبة', href: '/' },
+        { title: 'تصميم', href: '/' },
+        { title: 'مونتاج', href: '/' },
+      ],
+      footerLinks: [
+        { title: 'أسئلة شائعة', href: '/FAQ' },
+        { title: 'عنا', href: '/about' },
+        { title: 'مستقلين', href: '/freelancers' },
+        { title: 'وظائف', href: '/jobs' },
+        { title: 'الرئيسية', href: '/' },
+      ],
+      footerIcons: (color) => [
+        <Facebook key={0} sx={{ color: color, cursor: 'pointer' }} />,
+        <Twitter key={1} sx={{ color: color, cursor: 'pointer' }} />,
+        <LinkedIn key={2} sx={{ color: color, cursor: 'pointer' }} />,
+        <Instagram key={3} sx={{ color: color, cursor: 'pointer' }} />,
+      ],
+      testimonialsCards: [
+        {
+          title: 'يوسف مصطفي',
+          subTitle: 'مبرمج فرونت أند',
+          body: '“للبقاء في طليعة ابتكارات تكنولوجيا المعلومات ، يحتاج فريقك إلى إعادة ابتكار مجموعة مهاراته بانتظام. مع Udemy Business ، يمكنني منح فريقي مساحة للتعلم وأخذ زمام المبادرة. هذا يعني أنه يمكننا إنتاج أعمال عالية الجودة بسرعة أكبر .”',
+        },
+        {
+          title: 'يوسف مصطفي',
+          subTitle: 'مبرمج فرونت أند',
+          body: '“للبقاء في طليعة ابتكارات تكنولوجيا المعلومات ، يحتاج فريقك إلى إعادة ابتكار مجموعة مهاراته بانتظام. مع Udemy Business ، يمكنني منح فريقي مساحة للتعلم وأخذ زمام المبادرة. هذا يعني أنه يمكننا إنتاج أعمال عالية الجودة بسرعة أكبر .”',
+        },
+        {
+          title: 'يوسف مصطفي',
+          subTitle: 'مبرمج فرونت أند',
+          body: '“للبقاء في طليعة ابتكارات تكنولوجيا المعلومات ، يحتاج فريقك إلى إعادة ابتكار مجموعة مهاراته بانتظام. مع Udemy Business ، يمكنني منح فريقي مساحة للتعلم وأخذ زمام المبادرة. هذا يعني أنه يمكننا إنتاج أعمال عالية الجودة بسرعة أكبر .”',
+        },
+      ],
+      coursesTracks: [
+        ['statistics', 'إحصائي'],
+        ['robotics', 'مهندس روبوتات'],
+        ['dataAnalysis', 'علوم البيانات'],
+        ['accounting', 'محاسبه'],
+        ['business', 'إدارة اعمال'],
+        ['programming', 'مهندس برمجيات'],
+      ],
+      coursesTracksCards: {
+        programming: [
+          { title: 'كورس البرمجة', subTitle: 'مهندس برمجيات', ratings: 5 },
+          { title: 'كورس البرمجة', subTitle: 'مهندس برمجيات', ratings: 5 },
+          { title: 'كورس البرمجة', subTitle: 'مهندس برمجيات', ratings: 5 },
+          { title: 'كورس البرمجة', subTitle: 'مهندس برمجيات', ratings: 5 },
+          { title: 'كورس البرمجة', subTitle: 'مهندس برمجيات', ratings: 5 },
+          { title: 'كورس البرمجة', subTitle: 'مهندس برمجيات', ratings: 5 },
+        ],
+        business: [
+          { title: 'كورس إدارة الأعمال', subTitle: 'إدارة اعمال', ratings: 5 },
+          { title: 'كورس إدارة الأعمال', subTitle: 'إدارة اعمال', ratings: 5 },
+          { title: 'كورس إدارة الأعمال', subTitle: 'إدارة اعمال', ratings: 5 },
+          { title: 'كورس إدارة الأعمال', subTitle: 'إدارة اعمال', ratings: 5 },
+          { title: 'كورس إدارة الأعمال', subTitle: 'إدارة اعمال', ratings: 5 },
+          { title: 'كورس إدارة الأعمال', subTitle: 'إدارة اعمال', ratings: 5 },
+        ],
+        accounting: [
+          { title: 'كورس المحاسبة', subTitle: 'محاسبه', ratings: 5 },
+          { title: 'كورس المحاسبة', subTitle: 'محاسبه', ratings: 5 },
+          { title: 'كورس المحاسبة', subTitle: 'محاسبه', ratings: 5 },
+          { title: 'كورس المحاسبة', subTitle: 'محاسبه', ratings: 5 },
+          { title: 'كورس المحاسبة', subTitle: 'محاسبه', ratings: 5 },
+          { title: 'كورس المحاسبة', subTitle: 'محاسبه', ratings: 5 },
+        ],
+        dataAnalysis: [
+          {
+            title: 'كورس علوم البيانات',
+            subTitle: 'علوم البيانات',
+            ratings: 5,
+          },
+          {
+            title: 'كورس علوم البيانات',
+            subTitle: 'علوم البيانات',
+            ratings: 5,
+          },
+          {
+            title: 'كورس علوم البيانات',
+            subTitle: 'علوم البيانات',
+            ratings: 5,
+          },
+          {
+            title: 'كورس علوم البيانات',
+            subTitle: 'علوم البيانات',
+            ratings: 5,
+          },
+          {
+            title: 'كورس علوم البيانات',
+            subTitle: 'علوم البيانات',
+            ratings: 5,
+          },
+          {
+            title: 'كورس علوم البيانات',
+            subTitle: 'علوم البيانات',
+            ratings: 5,
+          },
+        ],
+        robotics: [
+          { title: 'كورس الروبوتات', subTitle: 'مهندس روبوتات', ratings: 5 },
+          { title: 'كورس الروبوتات', subTitle: 'مهندس روبوتات', ratings: 5 },
+          { title: 'كورس الروبوتات', subTitle: 'مهندس روبوتات', ratings: 5 },
+          { title: 'كورس الروبوتات', subTitle: 'مهندس روبوتات', ratings: 5 },
+          { title: 'كورس الروبوتات', subTitle: 'مهندس روبوتات', ratings: 5 },
+          { title: 'كورس الروبوتات', subTitle: 'مهندس روبوتات', ratings: 5 },
+        ],
+        statistics: [
+          { title: 'كورس تحليل البيانات', subTitle: 'إحصائي', ratings: 5 },
+          { title: 'كورس تحليل البيانات', subTitle: 'إحصائي', ratings: 5 },
+          { title: 'كورس تحليل البيانات', subTitle: 'إحصائي', ratings: 5 },
+          { title: 'كورس تحليل البيانات', subTitle: 'إحصائي', ratings: 5 },
+          { title: 'كورس تحليل البيانات', subTitle: 'إحصائي', ratings: 5 },
+          { title: 'كورس تحليل البيانات', subTitle: 'إحصائي', ratings: 5 },
+        ],
+      },
+    },
+  },
 });
 
 export default constantsSlice.reducer;
