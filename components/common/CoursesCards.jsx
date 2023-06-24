2ef00245cd31a86834f51ef3445f53905f4281b5
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-06-24 15:50:51 +0300

    substring to courses card feature

--- components/common/CoursesCards.jsx
+++ components/common/CoursesCards.jsx
@@ -12,50 +12,59 @@ import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 
 const CoursesCards = ({ content }) => {
-    const ratings = Math.floor((Math.random() * 5) + 1);
+  const ratings = Math.floor(Math.random() * 5 + 1);
 
-    return (
-        <Card sx={{ maxWidth: 345, height: "fit-content" }}>
-            <CardHeader
-                sx={{px: 0}}
-                title={
-                    <Typography variant="h3" sx={{mr: 1.5, mb: 1}}>
-                        {content.course_name}
-                    </Typography>
-                }
-                subheader={
-                    <Box sx={{display: "flex", alignItems: "center", gap: "10px", px: 1.5}}>
-                        <Typography component="legend">التقييمات</Typography>
-                        <Rating size="small" name="read-only" value={ratings} readOnly />
-                        <Typography color="primary">
-                            {ratings}
-                        </Typography>
-                    </Box>
-                }
-            />
-            <CardMedia
-                sx={{ width: "100%" }}
-                component="img"
-                image={content.image}
-                title="SAP"
-            />
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    {content.description}
-                </Typography>
-            </CardContent>
-            <CardActions sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                <Link href={{pathname: `/course/${content.course_id}`}} style={{textDecoration: "none"}}>
-                    <Typography color="primary" sx={{cursor: "pointer"}}>
-                        شاهد
-                    </Typography>
-                </Link>
-                <IconButton aria-label="add to favorites">
-                    <Favorite />
-                </IconButton>
-            </CardActions>
-      </Card>
-    )
-}
+  return (
+    <Card sx={{ maxWidth: 345, height: "fit-content" }}>
+      <CardHeader
+        sx={{ px: 0 }}
+        title={
+          <Typography variant="h3" sx={{ mr: 1.5, mb: 1 }}>
+            {content.course_name.substring(0, 50)}
+          </Typography>
+        }
+        subheader={
+          <Box
+            sx={{ display: "flex", alignItems: "center", gap: "10px", px: 1.5 }}
+          >
+            <Typography component="legend">التقييمات</Typography>
+            <Rating size="small" name="read-only" value={ratings} readOnly />
+            <Typography color="primary">{ratings}</Typography>
+          </Box>
+        }
+      />
+      <CardMedia
+        sx={{ width: "100%" }}
+        component="img"
+        image={content.image}
+        title="SAP"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {content.description.substring(0, 150)}
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Link
+          href={{ pathname: `/course/${content.course_id}` }}
+          style={{ textDecoration: "none" }}
+        >
+          <Typography color="primary" sx={{ cursor: "pointer" }}>
+            شاهد
+          </Typography>
+        </Link>
+        <IconButton aria-label="add to favorites">
+          <Favorite />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default CoursesCards;
